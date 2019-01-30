0cadee5c79894bbfa000176d2e6683eda69977c0
Asgeir95 <asgeir_95@hotmail.com>
2019-01-30 23:13:09 +0100

    work in progress

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task2
+{
+    // class Ferry : Vessel { .. }
+    //class Tugboat : Vessel { .. }
+    //class Submarine : Vessel { .. }
+
+    class Vessel {
+        private string name;
+        private string yearBuilt;
+
+        public Vessel(string Name, string Year){
+            this.name = Name;
+            this.yearBuilt = Year;
+        }
+
+        public string GetName();
+        public string GetYearBuilt();
+
+        public override string ToString() {
+            return $"Vessel: {name}";
+        }
+
+        
+
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
